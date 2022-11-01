3c9c9a191c3e4512d3f0a36690b92d288ca9498c
Rustum Jordan <rustum@thinksmartsoftware.com>
2022-11-01 23:15:00 +0800

    add background color styling with snackbar

--- adoptionapp/lib/components/gsnackbar.dart
+++ adoptionapp/lib/components/gsnackbar.dart
@@ -6,9 +6,19 @@ class GSnackbar {
     required String type,
     required String message,
   }) {
+    MaterialAccentColor defaulBG = Colors.greenAccent;
+
+    if (type == 'error') {
+      defaulBG = Colors.redAccent;
+    }
+
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         content: Text(message),
+        backgroundColor: defaulBG,
+        behavior: SnackBarBehavior.floating,
+        margin: const EdgeInsets.fromLTRB(50, 25, 50, 25),
+        elevation: 30,
       ),
     );
   }
