8dbb52a8a9d2e923816a0c01eaa00398a5bf2f06
Rustum Jordan <rustum@thinksmartsoftware.com>
2022-11-03 23:12:11 +0800

    temporary design of home AppBar and SideMenu(Sidebar)

--- adoptionapp/lib/screens/home.dart
+++ adoptionapp/lib/screens/home.dart
@@ -6,11 +6,47 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Welcome Rustum Jordan'),
+      ),
+      drawer: SizedBox(
+        width: MediaQuery.of(context).size.width * 0.75, //20.0,
+        // padding: EdgeInsets.zero,
+        child: const Drawer(
+          child: side(),
+        ),
+      ),
       body: ListView(
         children: const [
           Text('You are now in Home', style: TextStyle(fontSize: 30))
         ],
       ),
+    );
+  }
+}
+
+class side extends StatelessWidget {
+  const side({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: const [
+        DrawerHeader(
+          decoration: BoxDecoration(
+            color: Colors.blue,
+          ),
+          child: Text('Welcome Rustum'),
+        ),
+        ListTile(
+          title: Text('Item 1'),
+        ),
+        ListTile(
+          title: Text('Item 2'),
+        ),
+      ],
     );
   }
 }
